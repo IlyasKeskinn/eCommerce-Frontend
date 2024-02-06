8e870d14bae558ae3ed8b0659014b60a3b680f01
Ilyas Keskin <ilyaskeskn8@gmail.com>
2024-02-06 12:46:27 +0100

    feat: Add dealTimer function with countdown display
    Integrate a JavaScript countdown timer function, dealTimer(), that updates the specified HTML elements with class "countdown-unit" to display the remaining days, hours, minutes, and seconds.

---
+++ src/js/dealtimer.js
@@ -0,0 +1,28 @@
+function dealTimer() {
+    const countDownUnits = document.querySelectorAll(".countdown-unit");
+
+    let timer = 10 * 24 * 60 * 60; // 10 days
+    const countDown = setInterval(() => {
+
+        let remainSecond = timer % 60;
+        let minute = Math.floor(timer % (60 * 60) / 60);
+        let hour = Math.floor(timer % (60 * 60 * 24) / (60 * 60));
+        let day = Math.floor(timer / (60 * 60 * 24));
+
+        countDownUnits[0].children[0].innerHTML = day < 10 ? `0${day}` : day;
+        countDownUnits[1].children[0].innerHTML = hour < 10 ? `0${hour}` : hour;
+        countDownUnits[2].children[0].innerHTML = minute < 10 ? `0${minute}` : minute;
+        countDownUnits[3].children[0].innerHTML = remainSecond < 10 ? `0${remainSecond}` : remainSecond;
+
+
+        console.log(day);
+        if (timer < 1) {
+            clearInterval(countDown);
+        } else {
+            timer--;
+        }
+    }, 1000);
+}
+
+
+dealTimer();
